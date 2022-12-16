d45eb4b0a340ad3d469fe64dddfd391ee9fcabc7
Zehra Punjwani <punjwanizee@gmail.com>
2022-12-16 15:52:21 +0000

    Area Responsive Container Chart  Data View with Sort, Filter and Search

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,257 @@
-import Tabular from "../Tabular";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { DropdownButton, Dropdown, ButtonGroup, Row, Col } from "react-bootstrap";
+import { setIntradayData, setIntradayError } from "../../redux/intradaySlice";
 import AreaResponsiveContainer from "../AreaResponsiveContainer";
+import Tabular from "../Tabular";
 
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+
+// const API_KEY = "GEIXYV4P44EIMBG6";
+// const GET_API = `https://www.alphavantage.co/query`;
 const Home = () => {
-    return (
-        <div style={{ textAlign: 'left' }}>
-            <div>Requirements</div>
-            <ul>
-                <li>Fetch time series data using the above api</li>
-                <li>Display tabular data</li>
-                <li>Visualise the data Nice to have</li>
-                <li>Ability to filter data and chart based on date/time range</li>
-                <li>Ability to switch stock symbol</li>
-            </ul>
+    const timeSeriesIntraDayData = useSelector((state) => state.intraday.timeSeriesIntraDayData);
+    const intradayData = useSelector((state) => state.intraday.intradayData);
 
-            <div style={{ marginBottom: '20px' }}>
-                <Tabular />
-            </div>
+    const metaData = {
+        "Series Type": ["TIME_SERIES_INTRADAY"],
+        "Symbol": ["IBM"],
+        "Last Refreshed": null,
+        "Interval": ["1MIN", "5MIN", "15MIN", "30MIN"],
+        "Output Size": null,
+        "Time Zone": null
+    };
+    const [tabularData, setTabularData] = useState({
+        SeriesType: metaData["Series Type"][0],
+        Symbol: metaData["Symbol"][2],
+        LastRefreshed: null,
+        Interval: metaData["Interval"][2],
+        Information: `Time Series (${metaData["Interval"][2].toLowerCase()})`,
+        OutputSize: null,
+        TimeZone: null,
+        data: [],
+        error: null,
+    });
+    const [refresh, setRefresh] = useState(false);
+
+    const [date, setDate] = useState("2022-11-15");
+
+    const dispatch = useDispatch();
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const sort = (data) => {
+        console.log(data);
+        data.map((entry) => {
+            Object.keys(entry).map((key) => {
+                if (key !== capitalizeFirstLetter(key.split(' ')[0])) {
+                    const newKey = capitalizeFirstLetter(key.split(' ')[1]);
+                    delete Object.assign(entry, {[newKey]: entry[key]})[key]
+                }
+            })
+        });
+
+        return data.sort((a,b) => (a["Timestamp"] > b["Timestamp"])
+            ? 1
+            : ((b["Timestamp"] > a["Timestamp"]) ? -1 : 0))
+    }
+
+    const groupData = (data) => {
+        const groupedData = data.reduce((entry, a) => {
+            const date = a["Timestamp"].split(' ')[0];
+            entry[date] = entry[date] || [];
+            entry[date].push(a);
+
+            return entry;
+        }, Object.create(null));
+
+        // console.log("groupedData", groupedData)
+        // console.log("groupedData", groupedData[date])
+
+        return groupedData;
+    }
+
+    // console.log("groupedData test", Object.values(groupData(tabularData.data)))
+
+    const fetchTimeSeriesIntradayData = async (props) => {
+        await setRefresh(true);
+
+        await dispatch(setIntradayData({ response: [] }));
+        await dispatch(setIntradayError({ error: null }));
+        await setTabularData({ ...tabularData, ...props });
+
+        let response;
+        switch(props['Interval']) {
+            case "1MIN":
+                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_1MIN;
+                dispatch(setIntradayData({ response: response }));
+                break;
+            case "5MIN":
+                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_5MIN;
+                dispatch(setIntradayData({ response: response }));
+                break;
+            case "15MIN":
+                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_15MIN;
+                dispatch(setIntradayData({ response: response }));
+                break;
+            case "30MIN":
+                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_30MIN;
+                dispatch(setIntradayData({ response: response }));
+                break;
+            default:
+                break;
+        }
+
+        if(response.hasOwnProperty("Meta Data")) {
+            let intradayDataKeys;
+            let intradayDataValues
+
+            if (refresh || Object.keys(response).length === 0) {
+                intradayDataKeys = Object.keys(Object.values(response)[1]);
+                intradayDataValues = Object.values(Object.values(response)[1]);
+            } else {
+                intradayDataKeys = Object.keys(Object.values(response)[1]);
+                intradayDataValues = Object.values(Object.values(response)[1]);
+            }
+
+            intradayDataValues.map((value, i) => {
+                return intradayDataValues[i] = {
+                    ...intradayDataValues[i],
+                    "0. timestamp": intradayDataKeys[i]
+                }
+            });
+
+            console.log("intradayDataValues", intradayDataValues);
+
+            // this gives an object with dates as keys
+            const groups = sort(intradayDataValues).reduce((groups, entry) => {
+                const date = entry["Timestamp"].split(' ')[0];
+                if (!groups[date]) {
+                    groups[date] = [];
+                }
+
+                const updatedEntry = { ...entry, "Timestamp": entry["Timestamp"] }
+                groups[date].push(updatedEntry);
+
+                return groups;
+            }, {});
+
+            console.log("groups", groups);
+            console.log("groups values", Object.values(groups).flat());
+
+            await setTabularData({
+                ...props,
+                LastRefreshed: response["Meta Data"]["3. Last Refreshed"],
+                OutputSize: response["Meta Data"]["5. Output Size"],
+                TimeZone: response["Meta Data"]["6. Time Zone"],
+                data: Object.values(groups).flat(),
+            });
+        } else {
+            if(response.hasOwnProperty("Error Message")) {
+                dispatch(setIntradayError({ error: response["Error Message"] }))
+                setTabularData({
+                    ...props,
+                    LastRefreshed: "N/A",
+                    OutputSize: "",
+                    TimeZone: "",
+                    error: response["Error Message"]
+                });
+            } else if(response.hasOwnProperty("Note")) {
+                dispatch(setIntradayError({ error: response["Note"]}))
+                setTabularData({
+                    ...props,
+                    LastRefreshed: "N/A",
+                    OutputSize: "",
+                    TimeZone: "",
+                    error: response["Note"]
+                });
+            } else {
+                dispatch(setIntradayError({ error: response["Information"]}))
+                setTabularData({
+                    ...props,
+                    LastRefreshed: "N/A",
+                    OutputSize: "",
+                    TimeZone: "",
+                    error: response["Information"]
+                });
+            }
+        }
+
+        await setRefresh(false);
+
+    }
+
+    useEffect(() => {
+        fetchTimeSeriesIntradayData(tabularData)
+    }, []);
+
+    return (
+        <div>
+            <Row>
+                <Col style={{ textAlign: 'right' }}>
+                    <ButtonGroup className="mb-2">
+                        <DropdownButton id="dropdown-basic-button" title={tabularData["SeriesType"]} style={{ marginLeft: '10px' }}>
+                            {
+                                metaData["Series Type"].map((seriesType) => (
+                                    <Dropdown.Item key={seriesType}>{seriesType}</Dropdown.Item>
+                                ))
+                            }
+                        </DropdownButton>
+                        <DropdownButton id="dropdown-basic-button" title={tabularData["Symbol"]} style={{ marginLeft: '10px' }}>
+                            {
+                                metaData["Symbol"].map((symbol) => (
+                                    <Dropdown.Item key={symbol}>{symbol}</Dropdown.Item>
+                                ))
+                            }
+                        </DropdownButton>
+                        <DropdownButton id="dropdown-basic-button" title={tabularData["Interval"]} style={{ marginLeft: '10px' }}>
+                            {
+                                metaData["Interval"].map((inter) => (
+                                    <Dropdown.Item
+                                        key={inter}
+                                        onClick={() => {
+                                            fetchTimeSeriesIntradayData({
+                                                ...tabularData,
+                                                "Interval": inter,
+                                                "Information": `Time Series (${inter.toLowerCase()})`,
+                                                "data": [],
+                                                "error": null,
+                                            })
+                                        }}
+                                    >{inter}</Dropdown.Item>
+                                ))
+                            }
+                        </DropdownButton>
+                        <DropdownButton id="dropdown-basic-button" title={date} style={{ marginLeft: '10px', textAlign: 'right' }}>
+                            {
+                                Object.keys(groupData(tabularData.data)).map((entry) => (
+                                    <Dropdown.Item key={entry}
+                                        onClick={async () => {
+                                            await setDate(entry)
+                                            console.log(entry)
+                                        }}>
+                                        {entry}
+                                    </Dropdown.Item>
+                                ))
+                            }
+                        </DropdownButton>
+                    </ButtonGroup>
+                </Col>
+            </Row>
 
             <div>
-                <AreaResponsiveContainer />
+                <Tabular tabularData={tabularData} groupData={groupData(tabularData.data)[date]} refresh={refresh} intradayData={intradayData} />
+            </div>
+
+            <div style={{ marginTop: "20px"}}>
+                <AreaResponsiveContainer data={groupData(tabularData.data)[date]} refresh={refresh} intradayData={intradayData} />
             </div>
         </div>
     )
 }
 
 export default Home;
+
--- src/components/Tabular/index.js
+++ src/components/Tabular/index.js
@@ -1,45 +1,13 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Container, Card, Button, Spinner, DropdownButton, Dropdown, ButtonGroup, Row, Col } from "react-bootstrap";
+import {Button, ButtonGroup, Card, Col, Container, Row, Spinner} from "react-bootstrap";
 import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit'
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
-import { setIntradayData, setIntradayError } from "../../redux/intradaySlice";
 
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
-
-// const API_KEY = "GEIXYV4P44EIMBG6";
-// const GET_API = `https://www.alphavantage.co/query`;
-const TimeSeriesIntraday = () => {
-    const timeSeriesIntraDayData = useSelector((state) => state.intraday.timeSeriesIntraDayData);
-    const intradayData = useSelector((state) => state.intraday.intradayData);
-
-    const metaData = {
-        "Series Type": ["TIME_SERIES_INTRADAY"],
-        "Symbol": ["IBM"],
-        "Last Refreshed": null,
-        "Interval": ["1MIN", "5MIN", "15MIN", "30MIN"],
-        "Output Size": null,
-        "Time Zone": null
-    };
-    const [tabularData, setTabularData] = useState({
-        SeriesType: metaData["Series Type"][0],
-        Symbol: metaData["Symbol"][0],
-        LastRefreshed: null,
-        Interval: metaData["Interval"][0],
-        Information: `Time Series (${metaData["Interval"][0].toLowerCase()})`,
-        OutputSize: null,
-        TimeZone: null,
-        data: [],
-        error: null,
-    });
-    const [refresh, setRefresh] = useState(false);
-
-    const dispatch = useDispatch();
+const Tabular = ({ tabularData, groupData, refresh, intradayData }) => {
 
     const headerFormatter = (column, colIndex, { sortElement, filterElement }) => {
+
         return (
             <div style={ { display: 'flex', flexDirection: 'column', fontSize: '14px' } }>
                 {/*{ column.text }*/}
@@ -72,6 +40,7 @@ const TimeSeriesIntraday = () => {
     );
 
     const customFilter = ({filterVal, data, col}) => {
+
         if (filterVal && data) {
             return data.filter(entry => entry[col].contains(filterVal));
         }
@@ -80,109 +49,15 @@ const TimeSeriesIntraday = () => {
 
     const MyExportCSV = (props) => (
         <Button
-            className={tabularData.data.length === 0 ? "btn btn-secondary" : "btn btn-info"}
+            className={groupData.length === 0 ? "btn btn-secondary" : "btn btn-info"}
             onClick={() => props.onExport()}
-            disabled={tabularData.data.length === 0}
+            disabled={groupData.length === 0}
         >Export to CSV</Button>
     )
 
-    const fetchTimeSeriesIntradayData = async (props) => {
-        await setRefresh(true);
-
-        await dispatch(setIntradayData({ response: [] }));
-        await dispatch(setIntradayError({ error: null }));
-        await setTabularData({ ...tabularData, ...props });
-
-        let response;
-        switch(props['Interval']) {
-            case "1MIN":
-                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_1MIN;
-                dispatch(setIntradayData({ response: response }));
-                break;
-            case "5MIN":
-                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_5MIN;
-                dispatch(setIntradayData({ response: response }));
-                break;
-            case "15MIN":
-                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_15MIN;
-                dispatch(setIntradayData({ response: response }));
-                break;
-            case "30MIN":
-                response = timeSeriesIntraDayData.TIME_SERIES_INTRADAY_IBM_30MIN;
-                dispatch(setIntradayData({ response: response }));
-                break;
-            default:
-                break;
-        }
-
-        if(response.hasOwnProperty("Meta Data")) {
-            let intradayDataKeys;
-            let intradayDataValues
-
-            if (refresh || Object.keys(response).length === 0) {
-                intradayDataKeys = Object.keys(Object.values(response)[1]);
-                intradayDataValues = Object.values(Object.values(response)[1]);
-            } else {
-                intradayDataKeys = Object.keys(Object.values(response)[1]);
-                intradayDataValues = Object.values(Object.values(response)[1]);
-            }
-
-            intradayDataValues.map((value, i) => {
-                return intradayDataValues[i] = {
-                    ...intradayDataValues[i],
-                    "0. timestamp": intradayDataKeys[i]
-                }
-            });
-
-            await setTabularData({
-                ...props,
-                LastRefreshed: response["Meta Data"]["3. Last Refreshed"],
-                OutputSize: response["Meta Data"]["5. Output Size"],
-                TimeZone: response["Meta Data"]["6. Time Zone"],
-                data: intradayDataValues,
-            });
-        } else {
-            if(response.hasOwnProperty("Error Message")) {
-                dispatch(setIntradayError({ error: response["Error Message"] }))
-                setTabularData({
-                    ...props,
-                    LastRefreshed: "N/A",
-                    OutputSize: "",
-                    TimeZone: "",
-                    error: response["Error Message"]
-                });
-            } else if(response.hasOwnProperty("Note")) {
-                dispatch(setIntradayError({ error: response["Note"]}))
-                setTabularData({
-                    ...props,
-                    LastRefreshed: "N/A",
-                    OutputSize: "",
-                    TimeZone: "",
-                    error: response["Note"]
-                });
-            } else {
-                dispatch(setIntradayError({ error: response["Information"]}))
-                setTabularData({
-                    ...props,
-                    LastRefreshed: "N/A",
-                    OutputSize: "",
-                    TimeZone: "",
-                    error: response["Information"]
-                });
-            }
-        }
-
-        await setRefresh(false);
-
-    }
-
-    useEffect(() => {
-        fetchTimeSeriesIntradayData(tabularData)
-    }, []);
-
     const columns = [
         {
-            dataField: '0. timestamp',
+            dataField: 'Timestamp',
             text: 'Timestamp',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -193,7 +68,7 @@ const TimeSeriesIntraday = () => {
 
         },
         {
-            dataField: '1. open',
+            dataField: 'Open',
             text: 'Open',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -204,7 +79,7 @@ const TimeSeriesIntraday = () => {
             }
         },
         {
-            dataField: '2. high',
+            dataField: 'High',
             text: 'High',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -215,7 +90,7 @@ const TimeSeriesIntraday = () => {
             }
         },
         {
-            dataField: '3. low',
+            dataField: 'Low',
             text: 'Low',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -226,7 +101,7 @@ const TimeSeriesIntraday = () => {
             }
         },
         {
-            dataField: '4. close',
+            dataField: 'Close',
             text: 'Close',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -237,7 +112,7 @@ const TimeSeriesIntraday = () => {
             }
         },
         {
-            dataField: '5. volume',
+            dataField: 'Volume',
             text: 'Volume',
             sort: true,
             filter: textFilter({ onFilter: customFilter }),
@@ -251,7 +126,7 @@ const TimeSeriesIntraday = () => {
     const loadContent = (props) => {
         if(refresh) {
             return (
-                <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '25vh' }}>
+                <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '25vh', minWidth: '100%' }}>
                     <Spinner animation="border" role="status">
                         <span className="visually-hidden">Loading...</span>
                     </Spinner>
@@ -260,7 +135,7 @@ const TimeSeriesIntraday = () => {
         } else {
             if (intradayData.error) {
                 return (
-                    <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '25vh' }}>
+                    <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '25vh', minWidth: '100%' }}>
                         <Card bg="danger" text='light' style={{width: '100%'}} className="mb-2">
                             <Card.Body>
                                 <Card.Title>Error</Card.Title>
@@ -274,7 +149,7 @@ const TimeSeriesIntraday = () => {
                     <div style={{ marginTop: '10px', padding: '0px' }}>
                         <BootstrapTable
                             {...props.baseProps}
-                            defaultSorted={[{ dataField: '0. timestamp', order: 'desc' }]}
+                            defaultSorted={[{ dataField: 'Timestamp', order: 'desc' }]}
                             filter={filterFactory()}
                             pagination={paginationFactory({sizePerPageRenderer})}
                             noDataIndication={ 'no results found' }
@@ -289,7 +164,7 @@ const TimeSeriesIntraday = () => {
         <Card>
             <Card.Header>
                 <Row>
-                    <Col>
+                    <Col style={{ textAlign: 'left' }}>
                         TIME_SERIES_INTRADAY Tabular
                     </Col>
                     <Col style={{ textAlign: 'right' }}>
@@ -298,54 +173,16 @@ const TimeSeriesIntraday = () => {
                 </Row>
             </Card.Header>
             <Card.Body>
-                <ToolkitProvider keyField='0. timestamp' data={tabularData.data} columns={columns} exportCSV={ { onlyExportFiltered: true, exportAll: false } }>
+                <ToolkitProvider keyField='Timestamp' data={groupData} columns={columns} exportCSV={ { onlyExportFiltered: true, exportAll: false } }>
                     {props => (
-                        <div>
-                            <Row>
-                                <Col>
-                                    <ButtonGroup className="mb-2">
-                                        <MyExportCSV {...props.csvProps} />
-                                    </ButtonGroup>
-                                </Col>
-                                <Col style={{ textAlign: 'right' }}>
-                                    <ButtonGroup className="mb-2">
-                                        <DropdownButton id="dropdown-basic-button" title={tabularData["SeriesType"]} style={{marginLeft: '10px'}}>
-                                            {
-                                                metaData["Series Type"].map((seriesType) => (
-                                                    <Dropdown.Item key={seriesType}>{seriesType}</Dropdown.Item>
-                                                ))
-                                            }
-                                        </DropdownButton>
-                                        <DropdownButton id="dropdown-basic-button" title={tabularData["Symbol"]} style={{marginLeft: '10px'}}>
-                                            {
-                                                metaData["Symbol"].map((symbol) => (
-                                                    <Dropdown.Item key={symbol}>{symbol}</Dropdown.Item>
-                                                ))
-                                            }
-                                        </DropdownButton>
-                                        <DropdownButton id="dropdown-basic-button" title={tabularData["Interval"]} style={{marginLeft: '10px'}}>
-                                            {
-                                                metaData["Interval"].map((inter) => (
-                                                    <Dropdown.Item
-                                                        key={inter}
-                                                        onClick={() => {
-                                                            fetchTimeSeriesIntradayData({
-                                                                ...tabularData,
-                                                                "Interval": inter,
-                                                                "Information": `Time Series (${inter.toLowerCase()})`,
-                                                                "data": [],
-                                                                "error": null,
-                                                            })
-                                                        }}
-                                                    >{inter}</Dropdown.Item>
-                                                ))
-                                            }
-                                        </DropdownButton>
-                                    </ButtonGroup>
-                                </Col>
-                            </Row>
-                            {loadContent(props)}
-                        </div>
+                        <Row>
+                            <Col style={{ textAlign: 'left' }}>
+                                <ButtonGroup className="mb-2">
+                                    <MyExportCSV {...props.csvProps} />
+                                </ButtonGroup>
+                                {loadContent(props)}
+                            </Col>
+                        </Row>
                     )}
                 </ToolkitProvider>
             </Card.Body>
@@ -353,4 +190,4 @@ const TimeSeriesIntraday = () => {
     )
 }
 
-export default TimeSeriesIntraday;
+export default Tabular
